d5b9dd8bcb6bb8fd2da6bc15f8efd07b4e651f49
Apoorv Sohal <apoorvsohal@protonmail.com>
2023-12-08 11:17:12 +0530

    added vim rhubarb

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -3,6 +3,7 @@ return {
     "romgrk/nvim-treesitter-context",
     "ryanoasis/vim-devicons",
     "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
     "mbbill/undotree",
     -- Themes
     "folke/tokyonight.nvim", -- TOOOOOKKKKIIIIOOOOOO
