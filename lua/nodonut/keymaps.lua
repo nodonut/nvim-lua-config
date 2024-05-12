912681e5bd7ef29996e520e18cb07f53a1e3dedc
apoorv <apoorvsohal@protonmail.com>
2024-05-12 16:46:57 +0530

    formatting

--- lua/nodonut/keymaps.lua
+++ lua/nodonut/keymaps.lua
@@ -1,7 +1,7 @@
 local opts = { noremap = true, silent = true }
 
 local keymap = function(mode, keys, func)
-    vim.keymap.set(mode, keys, func, opts)
+	vim.keymap.set(mode, keys, func, opts)
 end
 
 keymap("", "<Space>", "<Nop>")
@@ -28,12 +28,12 @@ keymap("n", "<leader>ga", "<cmd>Telescope git_branches<cr>")
 keymap("n", "<leader>gs", "<cmd>Telescope git_status<cr>")
 keymap("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>")
 keymap("n", "<leader>?", "<cmd>Telescope oldfiles<cr>")
-keymap('n', '<leader>/', function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find()
+keymap("n", "<leader>/", function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find()
 end)
-keymap('n', "<leader>sc", function()
-    require('telescope.builtin').grep_string()
+keymap("n", "<leader>sc", function()
+	require("telescope.builtin").grep_string()
 end)
 
 -- Vim Test keymaps
@@ -50,10 +50,10 @@ keymap("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
 keymap("n", "<leader>rl", '<cmd>let @+ = expand("%")<cr>')
 
 -- NvimTree keymaps
-keymap("n", "<leader>nf", '<cmd>NvimTreeFindFileToggle<cr>')
+keymap("n", "<leader>nf", "<cmd>NvimTreeFindFileToggle<cr>")
 
 -- Prettier keymaps
-keymap("n", "<leader>fo", '<cmd>Prettier<cr>')
+keymap("n", "<leader>fo", "<cmd>Prettier<cr>")
 
 -- Harpoon keymaps
 keymap("n", "<leader>ha", '<cmd>lua require("harpoon.mark").add_file()<cr>')
@@ -63,13 +63,25 @@ keymap("n", "<leader>hN", '<cmd>lua require("harpoon.ui").nav_prev()<cr>')
 keymap("n", "<leader>hx", '<cmd>lua require("harpoon.mark").clear_all()<cr>')
 
 -- Git Diff
-keymap("n", "<leader>gj", '<cmd>diffget //3<cr>')
-keymap("n", "<leader>gf", '<cmd>diffget //2<cr>')
+keymap("n", "<leader>gj", "<cmd>diffget //3<cr>")
+keymap("n", "<leader>gf", "<cmd>diffget //2<cr>")
 
 -- Trouble
-keymap("n", "<leader>xx", function() require("trouble").toggle() end)
-keymap("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-keymap("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-keymap("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-keymap("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-keymap("n", "gR", function() require("trouble").toggle("lsp_references") end)
+keymap("n", "<leader>xx", function()
+	require("trouble").toggle()
+end)
+keymap("n", "<leader>xw", function()
+	require("trouble").toggle("workspace_diagnostics")
+end)
+keymap("n", "<leader>xd", function()
+	require("trouble").toggle("document_diagnostics")
+end)
+keymap("n", "<leader>xq", function()
+	require("trouble").toggle("quickfix")
+end)
+keymap("n", "<leader>xl", function()
+	require("trouble").toggle("loclist")
+end)
+keymap("n", "gR", function()
+	require("trouble").toggle("lsp_references")
+end)
