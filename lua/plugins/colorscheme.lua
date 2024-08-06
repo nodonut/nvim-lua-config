83981d8c48f900855ba9f493da869d28ee617554
apoorv <apoorvsohal@protonmail.com>
2024-08-06 18:25:41 +0530

    back on catpuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,7 +17,7 @@ return {
 		})
 
 		function ColorMyPencils(color)
-			color = color or "tokyonight"
+			color = color or "catppuccin"
 			vim.cmd.colorscheme(color)
 
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
